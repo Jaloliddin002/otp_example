7cb56b9c142b99369e125c2f7d086fb26ca60a34
Jaloliddin <jaloliddindeveloper@gmail.com>
2023-12-01 18:30:53 +0500

    created

---
+++ src/main/kotlin/uz/spring/otp_example/OtpExampleApplication.kt
@@ -0,0 +1,12 @@
+package uz.spring.otp_example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OtpExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<OtpExampleApplication>(*args)
+
+}
---
+++ src/test/kotlin/uz/spring/otp_example/OtpExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package uz.spring.otp_example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OtpExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
